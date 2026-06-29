75fd7ff8f1c52fee296eb3a735df576d61fe8eda
Kushmeshand <ktanuj820@gmail.com>
2026-06-29 22:19:23 +0530

    Time: 14 ms (74.5%), Space: 27 MB (32.24%) - LeetHub

--- 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -2,31 +2,38 @@ class Solution {
 public:
     int maxDistance(vector<vector<int>>& grid) {
         int n = grid.size();
-        int dist=0, size;
-        queue<pair<int, int>> q;
-        int drow[] = {0, -1, 0, 1}, dcol[] = {-1, 0, 1, 0};
-        for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++)    
-                if(grid[i][j] == 1)
-                    q.push({i, j});
-        if(q.size() == n*n)
-            return -1;
-        
-        while(!q.empty()){
-            size = q.size();
-            dist++;
-            while(size--){
-                int r = q.front().first, c = q.front().second;
-                q.pop();
-                for(int i=0;i<4;i++){
-                    int nrow = r + drow[i], ncol = c + dcol[i];
-                    if(nrow>=0 && ncol>=0 && nrow<n && ncol<n && grid[nrow][ncol]==0){
-                        grid[nrow][ncol] = 1;
-                        q.push({nrow, ncol});
-                    } 
+        int m = grid[0].size();
+        int maxi = -1;
+        vector<vector<int>> visited(n,vector<int>(m,0));
+        queue<pair<pair<int,int>,int>> q;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < m; j++){
+                if(grid[i][j]==1){
+                    q.push({{i,j},0});
+                    visited[i][j] = 1;
                 }
             }
         }
-        return dist-1;
+        if(q.empty() || q.size() == n*m) return -1;
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,-1,0,1};
+        while(!q.empty()){
+            int row = q.front().first.first;
+            int col = q.front().first.second;
+            int steps = q.front().second;
+            q.pop();
+
+            for(int i = 0; i < 4; i++){
+                int nrow = row + delrow[i];
+                int ncol = col + delcol[i];
+                if(nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && visited[nrow][ncol]!=1 && 
+                grid[nrow][ncol] == 0){
+                    q.push({{nrow,ncol},steps + 1});
+                    visited[nrow][ncol] = 1; 
+                }
+            }
+            maxi = max(steps,maxi);
+        }
+        return maxi;
     }
 };
